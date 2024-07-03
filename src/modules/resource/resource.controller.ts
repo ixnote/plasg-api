abc3b25bf91b0a0523e82863a00a14eaada2fcb6
Ukeh Victor <victorukeh1@gmail.com>
2024-07-03 10:49:44 +0100

    UPDATE: Add type tags get

--- src/modules/resource/resource.controller.ts
+++ src/modules/resource/resource.controller.ts
@@ -19,7 +19,7 @@ export class ResourceController {
 
   @Post('/create')
   @UseGuards(AuthGuard, RolesGuard)
-  @Roles(UserRoles.MDA, UserRoles.SUPER)
+  @Roles(UserRoles.MDA)
   async createResource(
     @Body() body: CreateResourceDto,
     @UserGuard() user: User,
